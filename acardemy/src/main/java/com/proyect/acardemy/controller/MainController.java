94a19c8658a02fa6a553d14df4197158cce22abb
julian cantillo <juliancortina009@gmail.com>
2025-10-22 19:15:42 -0500

    nuevos metodos para paginas  agregadas

--- acardemy/src/main/java/com/proyect/acardemy/controller/MainController.java
+++ acardemy/src/main/java/com/proyect/acardemy/controller/MainController.java
@@ -6,14 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     
-    @GetMapping("/")
-    public String inicio() {
-        return "index/index";
-    }
-
     @GetMapping("/index")
     public String Inicio() {
         return "index/index";
+    }
+
+    @GetMapping("/terminos")
+    public String TerminosCondiciones() {
+        return "index/terminos";
+    }
+
+    @GetMapping("/politicas")
+    public String PoliticasPrivacidad() {
+        return "index/politicas";
+    }
+
+    @GetMapping("/contactos")
+    public String Contactos() {
+        return "index/contactos";
+    }
+
+    @GetMapping("/acerca")
+    public String Acerca() {
+        return "index/acerca";
     }
 
 }
